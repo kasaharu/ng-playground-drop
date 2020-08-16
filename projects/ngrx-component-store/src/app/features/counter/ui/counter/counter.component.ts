3b97c95b84b1fe357ea31258397a38b5d6a0e7b1
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-16 10:25:38 +0900

    feat(ngrx-component-store): Display the default count using CounterStore

--- projects/ngrx-component-store/src/app/features/counter/ui/counter/counter.component.ts
+++ projects/ngrx-component-store/src/app/features/counter/ui/counter/counter.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CounterStore } from '../../store/counter.store';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [CounterStore],
 })
 export class CounterComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly counterStore: CounterStore) {}
+
+  readonly count$ = this.counterStore.count$;
 
   ngOnInit(): void {}
 }
