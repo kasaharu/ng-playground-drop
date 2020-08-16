4ca404d8e066bfc1807581aab4d6c4008c318bf0
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-16 10:01:59 +0900

    feat(ngrx-component-store): Link to counter page

--- projects/ngrx-component-store/src/app/app-routing.module.ts
+++ projects/ngrx-component-store/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopComponent } from './features/app-shell/ui/top/top.component';
+import { CounterComponent } from './features/counter/ui/counter/counter.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/top', pathMatch: 'full' },
   { path: 'top', component: TopComponent },
+  { path: 'counter', component: CounterComponent },
 ];
 
 @NgModule({
