4b1ba676eca6643f906eb6ea24748f0d4670584e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-16 09:52:53 +0900

    yarn ng g c features/app-shell/ui/top --project ngrx-component-store

---
+++ projects/ngrx-component-store/src/app/features/app-shell/ui/top/top.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-top',
+  templateUrl: './top.component.html',
+  styleUrls: ['./top.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
