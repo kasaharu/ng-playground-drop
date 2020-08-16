0121fa09ae8e2d19deb894b3d1b8b4dd7073eb88
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-16 10:00:36 +0900

    yarn ng g c features/counter/ui/counter --project ngrx-component-store

--- projects/ngrx-component-store/src/app/app.module.ts
+++ projects/ngrx-component-store/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './features/app-shell/ui/top/top.component';
+import { CounterComponent } from './features/counter/ui/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/ngrx-component-store/src/app/features/counter/ui/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
