ebef3917d384c2441fa704771997a2fe3ec3cd6c
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-23 21:08:14 +0900

    feat(dymanic-component-loader): Implement to load component

--- projects/dynamic-component-loader/src/app/components/ad-banner/ad-banner.component.ts
+++ projects/dynamic-component-loader/src/app/components/ad-banner/ad-banner.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ComponentFactoryResolver, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AdItem } from '../../ad-item';
+import { AdComponent } from '../../ad.component';
+import { AdDirective } from '../../directives/ad.directive';
 
 @Component({
   selector: 'app-ad-banner',
   templateUrl: './ad-banner.component.html',
   styleUrls: ['./ad-banner.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdBannerComponent implements OnInit {
+export class AdBannerComponent implements OnInit, OnDestroy {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+  @Input() ads!: AdItem[];
+  @ViewChild(AdDirective, { static: true }) adHost!: AdDirective;
 
-  constructor() { }
+  interval: any;
+  currentAdIndex = -1;
 
   ngOnInit(): void {
+    this.loadComponent();
+    this.getAds();
   }
 
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  getAds() {
+    this.interval = setInterval(() => {
+      console.log('test');
+    }, 3000);
+  }
+
+  loadComponent() {
+    this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
+    const adItem = this.ads[this.currentAdIndex];
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+    const viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent<AdComponent>(componentFactory);
+    componentRef.instance.data = adItem.data;
+  }
 }
